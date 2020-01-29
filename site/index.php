53bf7cb8046c87f20c3e6c7c4a2c37a45ef246a0
robbi <robbi@DESKTOP-52OKMHH>
2020-01-29 19:41:13 +0000

    -- added bouncing circles

---
+++ site/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<title>Coming Soon</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <canvas></canvas>
+    <div class="overlay"></div>
+    
+    <div class="header">
+        <h1>Jack Ellis Plastering</h1>
+    </div>
+    
+    
+    <script src="./js/script.js"></script>
+</body>
+</html>
